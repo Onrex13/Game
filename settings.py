3287408ae88525067f5d1d4804aeeaab1cfa4634
Onrex13 <91799581+Onrex13@users.noreply.github.com>
2023-12-20 09:15:45 +0100

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                                  ',
+'                                  ',
+'                                  ',
+' XX     XXX                  XX   ',
+' XX                               ',
+' XXXX           XX             XX ',
+' XXXX         XX                  ',
+' XX     X   XXXX      XX   XX     ',
+'     P  X   XXXX      XX   XXX    ',
+'     XXXX   XXXXXX    XX   XXXX   ',
+'XXXXXXXXX   XXXXXX    XX   XXXX   ']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
---
+++ tiles.py
@@ -0,0 +1,15 @@
+import pygame
+import random
+
+
+class Tile(pygame.sprite.Sprite):
+
+    def __init__(self, pos, size):
+        super().__init__()
+        self.image = pygame.Surface((size, size))
+        self.color = "grey"
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect(topleft=pos)
+
+    def update(self, x_shift):
+        self.rect.x += x_shift
